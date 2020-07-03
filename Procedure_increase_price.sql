54d7a32e3af749b8650cf4f33b7b095fc1ba1119
toz015 <42127682+toz015@users.noreply.github.com>
2020-07-03 19:33:40 -0400

    Delete Procedure_increase_price.sql

--- Procedure_increase_price.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `increase_price`(IN product_name VARCHAR(70), percentage DECIMAL)
-BEGIN
-	UPDATE products
-	SET buyPrice = buyPrice * (1 + percentage)
-	WHERE productName = product_name;
-    
-
-END
